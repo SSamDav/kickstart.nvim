91631e379da758ef2c6f8e59ba92c2cee5865530
Samuel Arcadinho <ssam_dav@hotmail.com>
2025-01-04 11:29:50 +0000

    Add python tools

--- init.lua
+++ init.lua
@@ -451,6 +451,23 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'zbirenbaum/copilot-cmp',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot_cmp').setup()
+    end,
+    dependencies = {
+      'zbirenbaum/copilot.lua',
+      cmd = 'Copilot',
+      config = function()
+        require('copilot').setup {
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+        }
+      end,
+    },
+  },
   { 'Bilal2453/luvit-meta', lazy = true },
   {
     -- Main LSP Configuration
@@ -711,7 +728,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -827,6 +844,7 @@ require('lazy').setup({
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
           },
+          { name = 'copilot' },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
@@ -918,8 +936,88 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  -----------------------------------------------------------------------------
+  -- DEBUGGING
 
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+  -- DAP Client for nvim
+  -- - start the debugger with `<leader>dc`
+  -- - add breakpoints with `<leader>db`
+  -- - terminate the debugger `<leader>dt`
+  {
+    'mfussenegger/nvim-dap',
+    keys = {
+      {
+        '<leader>dc',
+        function()
+          require('dap').continue()
+        end,
+        desc = 'Start/Continue Debugger',
+      },
+      {
+        '<leader>db',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Add Breakpoint',
+      },
+      {
+        '<leader>dt',
+        function()
+          require('dap').terminate()
+        end,
+        desc = 'Terminate Debugger',
+      },
+    },
+  },
+
+  -- UI for the debugger
+  -- - the debugger UI is also automatically opened when starting/stopping the debugger
+  -- - toggle debugger UI manually with `<leader>du`
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    keys = {
+      {
+        '<leader>du',
+        function()
+          require('dapui').toggle()
+        end,
+        desc = 'Toggle Debugger UI',
+      },
+    },
+    -- automatically open/close the DAP UI when starting/stopping the debugger
+    config = function()
+      local listener = require('dap').listeners
+      listener.after.event_initialized['dapui_config'] = function()
+        require('dapui').open()
+      end
+      listener.before.event_terminated['dapui_config'] = function()
+        require('dapui').close()
+      end
+      listener.before.event_exited['dapui_config'] = function()
+        require('dapui').close()
+      end
+    end,
+  },
+
+  -- Configuration for the python debugger
+  -- - configures debugpy for us
+  -- - uses the debugpy installation from mason
+  {
+    'mfussenegger/nvim-dap-python',
+    dependencies = 'mfussenegger/nvim-dap',
+    config = function()
+      -- fix: E5108: Error executing lua .../Local/nvim-data/lazy/nvim-dap-ui/lua/dapui/controls.lua:14: attempt to index local 'element' (a nil value)
+      -- see: https://github.com/rcarriga/nvim-dap-ui/issues/279#issuecomment-1596258077
+      local dap, dapui = require 'dap', require 'dapui'
+      dapui.setup()
+      -- uses the debugypy installation by mason
+      local debugpyPythonPath = require('mason-registry').get_package('debugpy'):get_install_path() .. '/venv/bin/python3'
+      require('dap-python').setup(debugpyPythonPath, {}) ---@diagnostic disable-line: missing-fields
+    end,
+  },
+
+  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
